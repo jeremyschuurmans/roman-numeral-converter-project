42bab76e0ca09bee20a69cc5a8d96c2bb9205334
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-02-05 13:27:37 -0600

    add solution for 49

--- lib/converter.rb
+++ lib/converter.rb
@@ -1,5 +1,22 @@
 class Converter
   def convert(num)
-    'XLIX'
+    digits     = num.digits.reverse
+    ones_place = digits[1].to_i
+    tens_place = digits[0].to_i
+
+    case tens_place
+    when 4
+      tens_place = 'XL'
+    end
+
+    case ones_place
+    when 9
+      ones_place = 'IX'
+    when 4
+      ones_place = 'IV'
+    end
+
+    numeral = tens_place + ones_place
+    numeral
   end
 end
