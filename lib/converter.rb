bb67094e5b190d95451a66ff89d31dae267a8b96
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-02-05 12:56:58 -0600

    add test for converting 49 and #convert method to Converter class

--- lib/converter.rb
+++ lib/converter.rb
@@ -1,3 +1,5 @@
 class Converter
-
+  def convert(num)
+    'XLIX'
+  end
 end
--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -1,6 +1,9 @@
 require 'converter'
 
 RSpec.describe Converter, '#convert' do
-
-  
+  it 'takes in 49 and returns XLIX' do
+    convert_49 = Converter.new
+    result = convert_49.convert(49)
+    expect(result).to eq('XLIX')
+  end
 end
