dd7ad48f27c37c45768d00f703f70f72f88fe4be
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-02-07 16:39:41 -0600

    create test and implement solution for 49

--- lib/numeral_converter.rb
+++ lib/numeral_converter.rb
@@ -1,5 +1,34 @@
 class NumeralConverter
     def convert(numeral)
-        49
+        numerals = {
+            "I" => 1,
+            "V" => 5,
+            "X" => 10,
+            "L" => 50
+        }
+
+        values = []
+        
+        numeral_array = numeral.split("")
+        numeral_array.each do |numeral|
+            values << numerals.values_at(numeral)
+        end
+        
+        values.flatten!
+        
+        duplicate_value = {}
+
+        values.each do |value|
+            if duplicate_value[value]
+                values.delete(value)
+            else
+                duplicate_value[value] = 1
+            end
+            
+        end
+
+        result = values[0] - values[1]
+
+        result
     end
 end
