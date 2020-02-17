00b76e7ad1ab9a239164172ec527deba2efa5d76
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-02-17 13:23:56 -0600

    remove old code and commented out code

--- lib/numeral_converter.rb
+++ lib/numeral_converter.rb
@@ -53,42 +53,11 @@ class NumeralConverter
             ones_place = 0
         end
 
-        # values
-        # ones_place
-        # tens_place
-
         if ones_place > tens_place
             result = ones_place - tens_place
         else
             result = ones_place + tens_place
         end
         result
-        
-        # duplicate_value = {}
-
-        # values.each do |value|
-        #     if duplicate_value[value]
-        #         values.delete(value)
-        #     else
-        #         duplicate_value[value] = 1
-        #     end
-        # end
-        
-        # values.sort! {|a, b| b <=> a }
-
-        # if values.length == 2 && values[0] == 50
-        #     result = values[0] - values[1]
-        # elsif values.length == 3 && values[0] == 50
-        #     result = (values[0] - values[1]) + values[2]
-        #     result
-        # end
     end
 end
-
-# expected: 49
-# got: [50, 1]
-
-# expected: 45
-# got: [50, 10, 5]
-
-# 40: [50, 10]
