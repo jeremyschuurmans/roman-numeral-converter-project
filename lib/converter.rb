a6455a4c3cdc9e55d73eff41ad0651903e03c881
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-02-18 22:22:18 -0600

    add test and implementation for L to 50

--- lib/converter.rb
+++ lib/converter.rb
@@ -1,3 +1,18 @@
 class Converter
+    NUMERALS = {
+        "L" => 50
+    }
 
+    def convert(input)
+        result = 0
+        i = 0
+
+        while i < input.length
+            value = NUMERALS[input[i]]
+
+            result += value
+            i += 1
+        end
+        result
+    end
 end
--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -1,5 +1,13 @@
 require 'converter'
 
 RSpec.describe Converter, '#convert' do
+    cases = [
+        ["L", 50]
+    ]
 
+    cases.each do |numeral, value|
+        it 'returns the proper value when passed a roman numeral string' do
+            expect(Converter.new.convert(numeral)).to eq(value)
+        end
+    end
 end
