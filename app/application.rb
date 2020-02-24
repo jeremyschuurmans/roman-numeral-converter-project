bf82256a82a9c3cdc4122050157efd2e2ede9c22
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-02-24 00:55:59 -0600

    modify tests and converter to return and parse JSON

--- app/application.rb
+++ app/application.rb
@@ -9,12 +9,13 @@ class Application
             numeral = request.params["roman"]
 
             converted_numeral = Converter.new.convert_roman_to_arabic(numeral)
+            result            = JSON.parse(converted_numeral)
 
-            response.status = 200
-            response.write converted_numeral
+            response.status   = 200
+            response.write result["value"]
             response.finish
         else
-            response.status = 404
+            response.status   = 404
             response.write "Sorry, that route does not exist."
             response.finish
         end
