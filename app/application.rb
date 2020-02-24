f0a0890d4eba6672495f749e5cb3e5598e4b24af
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-02-23 23:05:06 -0600

    add test and implementation for L to 50

--- app/application.rb
+++ app/application.rb
@@ -1,3 +1,18 @@
+require_relative '../lib/converter'
+
 class Application
+    def call(env)
+        response = Rack::Response.new
+        request  = Rack::Request.new(env)
+
+        if request.path.match(/convert/)
+            numeral = request.params["roman"]
+
+            converted_numeral = Converter.new.convert_roman_to_arabic(numeral)
+
+            response.write converted_numeral
+            response.finish
+        end
+    end
 
 end
--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -2,5 +2,14 @@ require 'spec_helper'
 require_relative '../app/application'
 
 RSpec.describe Application, '#call' do
+    def app()
+        Application.new
+    end
 
+    it 'returns the proper value when passed a roman numeral via GET request' do
+        get '/convert?roman=L'
+
+        expect(last_response.status).to eq(200)
+        expect(last_response.body).to include("50")    
+    end
 end
